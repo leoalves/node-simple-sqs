67c04a86958d2a580e0b71c4e313072d79b64ab4
leoalves <hello@leoalves.net>
2018-10-12 12:24:43 -0400

    First Commit

--- build/src/example.js
+++ build/src/example.js
@@ -1,28 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const main_1 = require("./main");
-const manager = new main_1.default({
-    ovpnFile: './test.ovpn',
-    username: 'username',
-    password: 'password',
-    onStateChange: state => {
-        console.log(state);
-        if (state === 'CONNECTED') {
-            setTimeout(() => tslib_1.__awaiter(this, void 0, void 0, function* () {
-                console.log('after 60 seconds connected - reconnecting');
-                yield manager.disconnect();
-                yield manager.init();
-                yield manager.connect();
-            }), 60000);
-        }
+const queue = new main_1.default({
+    queueUrl: 'https://sqs.us-east-1.amazonaws.com/993260122582/test_queue'
+});
+queue.pull().then(data => {
+    console.log('pulled from queue');
+    console.log(JSON.stringify(data));
+    console.log('deleting requested message');
+    queue.delete(data.ReceiptHandle).then(data => {
+        console.log('message deleted: ' + data);
+    });
+});
+queue
+    .push({
+    Body: 'this is the body',
+    Attributes: {
+        url: 'this is a test url',
+        numberOfSeconds: 23
     }
-});
-const start = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-    yield manager.init();
-    yield manager.connect();
-});
-start().catch(err => {
-    console.log(err);
+})
+    .then(data => {
+    console.log('pushed to queue');
+    console.log(data);
 });
 //# sourceMappingURL=example.js.map
--- build/src/main.js
+++ build/src/main.js
@@ -1,157 +1,86 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const node_vpn_daemon_1 = require("node-vpn-daemon");
-const Telnet = require("telnet-client");
-const events_1 = require("events");
-const util = require("util");
-const waitMS = util.promisify(setTimeout);
-class Manager {
-    constructor({ ovpnFile = 'test.ovpn', username = 'test', password = 'test', onStateChange = undefined }) {
-        this.init = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.destroyListeners();
-            this.telnet = new Telnet();
-            this.openVpnEmitter = new events_1.EventEmitter();
-            if (yield this.Daemon.isRunning()) {
-                yield this.Daemon.kill();
+const AWS = require("aws-sdk");
+class SQS {
+    constructor({ region = 'us-east-1', queueUrl, apiVersion = 'latest' }) {
+        this.parseAWSAttributes = (attributes) => {
+            let res = {};
+            for (let property in attributes) {
+                if (attributes.hasOwnProperty(property)) {
+                    res[property] =
+                        attributes[property].DataType === 'Number'
+                            ? parseInt(attributes[property].StringValue)
+                            : attributes[property].StringValue;
+                }
             }
-            yield this.Daemon.start();
-        });
-        this.getState = () => {
-            console.log(this.stateInterval);
-            return this.state;
+            return res;
         };
-        this.changeState = (newState) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.state = newState;
-            this.openVpnEmitter.emit('state-change');
-        });
-        this.establishManagerConnection = (params) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.telnet.connect(Object.assign({ host: '127.0.0.1', port: 1337, shellPrompt: '', timeout: 5000 }, params));
-        });
-        this.clientPid = () => {
-            return this.pid;
-        };
-        this.managerConnectionReady = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.streamLog();
-            yield this.execute('pid');
-            yield this.execute('bytecount 1');
-            yield this.execute('log on all');
-            yield this.execute('state on');
-            yield this.execute('hold release');
-            yield this.execute(util.format('username "Auth" "%s"', this.username));
-            yield this.execute(util.format('password "Auth" "%s"', this.password));
-        });
-        this.setState = (state) => {
-            if (this.state !== state) {
-                this.changeState(state);
+        this.parseRequestAttributes = (attributes) => {
+            let res = {};
+            for (let property in attributes) {
+                if (attributes.hasOwnProperty(property)) {
+                    res[property] = {
+                        StringValue: attributes[property].toString(),
+                        DataType: isNaN(attributes[property]) ? 'String' : 'Number'
+                    };
+                }
             }
+            return res;
         };
-        this.destroyListeners = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.telnet &&
-                this.telnet.removeAllListeners &&
-                this.telnet.removeAllListeners();
-            this.telnet && this.telnet.hasOwnProperty('end') && this.telnet.end();
-            this.telnet &&
-                this.telnet.hasOwnProperty('destroy') &&
-                this.telnet.destroy();
-            this.telnet = false;
-            this.openVpnEmitter = false;
-        });
-        this.connect = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            this.setListeners();
-            yield this.establishManagerConnection();
-        });
-        this.disconnect = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.execute('signal SIGTERM');
-            yield waitMS(1500);
-        });
-        this.changeIp = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.execute('client-kill ' + this.pid);
-            yield waitMS(1500);
-            yield this.execute(util.format('username "Auth" "%s"', this.username));
-            yield this.execute(util.format('password "Auth" "%s"', this.password));
-        });
-        this.changeServer = (ovpnFile, credentials) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.disconnect();
-            if (credentials) {
-                this.username = credentials.username;
-                this.password = credentials.password;
-            }
-            if (ovpnFile) {
-                this.ovpnFile = ovpnFile;
-            }
-            yield this.init();
-            yield this.connect();
-        });
-        this.kill = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.execute('signal SIGTERM');
-        });
-        this.execute = cmd => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (this.telnet) {
-                        this.telnet.exec(cmd, resolve);
-                    }
-                    else {
-                        reject('no telnet instance');
-                    }
-                }, 1000);
-            });
+        this.parseResponse = (message) => {
+            return {
+                MessageId: message.MessageId,
+                ReceiptHandle: message.ReceiptHandle,
+                Body: message.Body,
+                Attributes: this.parseAWSAttributes(message.MessageAttributes)
+            };
         };
-        this.setListeners = () => {
-            this.telnet.on('end', () => {
-                this.openVpnEmitter.emit('end');
-            });
-            this.telnet.on('close', () => {
-                this.openVpnEmitter.emit('close');
-            });
-            this.telnet.on('error', error => {
-                this.openVpnEmitter.emit('error', error);
-            });
-            this.telnet.on('ready', this.managerConnectionReady);
-            if (this.onStateChange) {
-                this.openVpnEmitter.on('state-change', () => {
-                    this.onStateChange(this.state);
-                });
+        this.push = ({ Body, Attributes }) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            const message = yield this.sqs
+                .sendMessage({
+                QueueUrl: this.queueUrl,
+                MessageBody: Body,
+                MessageAttributes: this.parseRequestAttributes(Attributes)
+            })
+                .promise();
+            if (message) {
+                return message.MessageId;
             }
-            if (this.onError) {
-                this.openVpnEmitter.on('error', this.onError);
+            return undefined;
+        });
+        this.pull = () => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            const message = yield this.sqs
+                .receiveMessage({
+                QueueUrl: this.queueUrl,
+                AttributeNames: ['All'],
+                MessageAttributeNames: ['All'],
+                MaxNumberOfMessages: 1
+            })
+                .promise();
+            if (message && message.Messages && message.Messages.length > 0) {
+                return this.parseResponse(message.Messages[0]);
             }
-            this.openVpnEmitter.on('pid-set', () => {
-                console.log('pid-set: ' + this.pid);
-            });
-        };
-        this.streamLog = () => {
-            this.telnet.shell().then(stream => {
-                stream.on('data', (data) => {
-                    const log = data.toString();
-                    log.split('\n').forEach((line) => {
-                        if (line.indexOf('pid=') !== -1) {
-                            this.pid = parseInt(log.split('pid=')[1].trim());
-                            this.openVpnEmitter.emit('pid-set');
-                        }
-                        if (line.indexOf('>STATE:') !== -1) {
-                            const state = line
-                                .split('>STATE:')[1]
-                                .replace('\n', '')
-                                .trim()
-                                .split(',');
-                            this.setState(state[1]);
-                        }
-                    });
-                });
-            });
-        };
-        this.ovpnFile = ovpnFile;
-        this.username = username;
-        this.password = password;
-        this.Daemon = new node_vpn_daemon_1.default({ ovpnFile: this.ovpnFile });
-        this.onStateChange = onStateChange;
-        this.telnet = new Telnet();
-        this.openVpnEmitter = new events_1.EventEmitter();
-        this.pid = undefined;
-        this.stateInterval = 0;
+            return undefined;
+        });
+        this.delete = (ReceiptHandle) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+            const deleted = yield this.sqs
+                .deleteMessage({
+                QueueUrl: this.queueUrl,
+                ReceiptHandle
+            })
+                .promise();
+            if (deleted) {
+                return true;
+            }
+            return false;
+        });
+        this.sqs = new AWS.SQS({
+            region,
+            apiVersion
+        });
+        this.queueUrl = queueUrl;
     }
 }
-exports.default = Manager;
+exports.default = SQS;
 //# sourceMappingURL=main.js.map
